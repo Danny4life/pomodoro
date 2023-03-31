ae8d8494a53ae9a2afc002f675714a4f3eb4b5f1
Segun Osiki <osiki.segun@gmail.com>
2023-03-31 07:58:26 +0100

    install react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 //import Home from "./page/homepage/Home"
 import StopTimer from "./page/timer/StopTimer"
+import { BrowserRouter, Router, Routes } from "react-router-dom"
 
 function App() {
 
 
   return (
     <div className="App">
+      <BrowserRouter>
+      <Router>
+        <Routes index="/" element={ <StopTimer />} />
+      </Router>
+      </BrowserRouter>
       {/* <Home /> */}
-      <StopTimer />
+     
     </div>
   )
 }
